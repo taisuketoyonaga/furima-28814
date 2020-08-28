59c9d3dfec99a21c5ae31563b67f11b034f268e2
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-28 19:45:43 +0900

    保存するカラムの指定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permited_parameters
+
+  private
+
+  def configure_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_reading,:first_name_reading,:birthday])
+  end
+  
 end
 
