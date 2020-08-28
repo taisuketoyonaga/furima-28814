ba2e4026562640cadd3b421d53c283768121ed16
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-28 14:54:02 +0900

    usersテーブルの作成2
    encodingをutf8に変更後
    マイグレーションファイルを編集し
    マイグレーションを実施

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ before_action :basic_auth
 
 private
 
-def basic_auth
-  authenticate_or_repuest_with_http_basic do |username, password|
-    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-end
-
+  def basic_auth
+    authenticate_or_repuest_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 end
