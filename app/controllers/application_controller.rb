9c21a5bd9a4256dc99d8059121b37e6272b1a0a1
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-29 10:20:04 +0900

    ログイン、サインアップ後トップページに遷移する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permited_parameters
+  before_action :configure_permited_parameters, if: :devise_controller?
 
   private
 
   def configure_permited_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:family_name,:first_name,:family_name_reading,:first_name_reading,:birthday])
   end
-  
+
 end
 
