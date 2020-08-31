8df6459e05f85b0bc2f75b1b31bfdc7394247126
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-31 12:36:27 +0900

    商品画像が１枚必要

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-
+  #before_action :item_params, only: :new
   def index
   end
 
@@ -9,5 +9,11 @@ class ItemsController < ApplicationController
 
   def create
   end
+
+  #private
+
+  #def item_params
+   #params.require(:item).permit(:image,:name,:explain,:category_id,:delivery_fee_id,:status_id,:shopping_origin_id,:delivery_date_id,:price).merge(user_id: current_user.id)
+  #end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :buy
+  has_one_attached :image
+
+#バリデーションの記入
+ validates :image, presence: true
+
 end
