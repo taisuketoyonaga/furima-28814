9b03d1576420e09424c65e836a00eb7e8e5bdc04
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-08-31 11:31:05 +0900

    MVCの準備

---
+++ db/migrate/20200831021249_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.text        :image,               null: false
+      t.string      :name,                null: false
+      t.text        :explain,             null: false
+      t.integer     :category_id,         null: false
+      t.integer     :status_id,           null: false
+      t.integer     :delivery_fee_id,     null: false
+      t.integer     :shopping_origin_id,  null: false
+      t.integer     :delivery_date_id,    null: false
+      t.integer     :price,               null: false
+      t.references  :user,                null: false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
