cf33a66baec0fa4c9b5a0b9e058f96cf84620f2d
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-01 10:53:42 +0900

    手数料と販売益を非同期で表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,18 @@ class Item < ApplicationRecord
   has_one :buy
   has_one_attached :image
 
-#バリデーションの記入
- validates :image, presence: true
- validates :name, presence: true
+ #バリデーションの記入
+  validates :image,
+           :name,  presence: true
 
- validates :category_id, 
+  validates :category_id, 
            :status_id,
            :delivery_fee_id,
            :shopping_origin_id,
-           :delivery_date_id,
-           presence: true, numericality: { other_than: 1 } 
+           :delivery_date_id,  presence: true, numericality: { other_than: 1 } 
+                                                               #ここ↓↓は勘
+  with_options presence: true, format: { with: /\A[0-9]+\z/, minmum: 300, maximum: 9999999, message: 'Halh-width numbers' } do
+  validates :price
+  end
 end
+
