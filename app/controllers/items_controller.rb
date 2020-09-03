1243a9a3289170ef5223ef80707c3bfff304e707
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-03 14:48:40 +0900

    出品者のみが削除を実行できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if current_user.id == @item.user.id
     @item.destroy
     redirect_to root_path
+    end
   end
 
   private
