570e35ef3e5a94e357623b157a147b6a02febb58
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-03 15:04:50 +0900

    出品者のみが削除可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,9 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if current_user.id == @item.user.id
-    @item.destroy
-    redirect_to root_path
+    if current_user.id == @item.user.id && @item.destroy
+      redirect_to root_path
+    else
+      render :show
     end
   end
 
