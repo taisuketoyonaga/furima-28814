7ae0f95e1956e9ee1e5e41ea5e93ef314e5534b3
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-03 23:00:24 +0900

    購入画面で商品の情報を表示

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,19 @@
 class BuysController < ApplicationController
-
+  before_action :buys_params, only: :index
+ 
   def index
+   @item = Item.find(params[:item_id])
   end
 
   def create
   end
-  
+
+  private
+    
+
+  def buys_params
+    params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+
 end
