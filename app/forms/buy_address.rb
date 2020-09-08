c4f5fb2d77cc3b6bb27175473b6ea1b45c6e1217
taisuketoyonaga <69187127+taisuketoyonaga@users.noreply.github.com>
2020-09-08 18:06:00 +0900

    buy_addressモデルの単体テストコードの実装

--- app/forms/buy_address.rb
+++ app/forms/buy_address.rb
@@ -5,12 +5,12 @@ class BuyAddress
   validates :user_id, presence: true
   validates :item_id, presence: true
   validates :token, presence: true
-  validates :prefectures, presence: true
+  validates :prefectures, presence: true, numericality: { other_than: 0, message: "is not choosen" }
   validates :address, presence: true
   validates :city, presence: true
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Input in only number"}
+    validates :phone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
   end
 def save
   buy = Buy.create(item_id: item_id, user_id: user_id)
